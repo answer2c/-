4e945ee62dccd8c8567ba0b8581da85d65f862b2
ybc <>
2020-08-05 12:13:44 +0800

    bubble sort

--- sort/bubble_sort.c
+++ sort/bubble_sort.c
@@ -2,15 +2,15 @@
 int bubble_sort(int *, int);
 
 int main() {
-    int arr[] = {1,2,4,7,9,0,9};
-    bubble_sort(arr, 7);
+    int arr[] = {1,9,5,0,2};
+    bubble_sort(arr, 5);
     return 0;
 }
 
 int bubble_sort(int *arr, int length)
 {
     int i,j;
-    //由低到高
+    //往后往前
     for (i=0; i < length; i++) {
         for (j=0; j < length-i-1; j++) {
             if (arr[j] > arr[j+1]) {
@@ -22,24 +22,25 @@ int bubble_sort(int *arr, int length)
     }
 
     for(int m=0; m < length; m++) {
-        printf("%d", arr[m]);
+        printf("%d ", arr[m]);
     }
     
     printf("\n");
-    //由高到低
-    for (i=0; i< length; i++) {
-        for (j=0; j<length-1; j++) {
-            if (arr[j+1] > arr[j]) {
-                int temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
+    //从前往后
+    for (i=1;i<length;i++) {
+        for (j=length-1;j>i-1;j--) {
+            if (arr[j] < arr[j-1]) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
             }
         }
     }
 
     for(int m=0; m < length; m++) {
-        printf("%d", arr[m]);
+        printf("%d ", arr[m]);
     }
+    printf("\n");
 
     return 0;
 }
